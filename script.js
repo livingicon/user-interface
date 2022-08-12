3f04bd9c08091b2bd9a5f0a59f8561391038dcee
Josh Hancock <livingicon@gmail.com>
2022-08-12 19:17:14 -0400

    Add image carousel

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// HAMBURGER MENU
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -13,3 +14,30 @@ document.querySelectorAll('.nav-link').forEach(link => link.
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
 }));
+
+
+
+
+// IMAGE CAROUSEL
+const carouselImg = document.querySelector('.carouselImg');
+const carouselBtn = document.querySelectorAll('.carouselBtn');
+const numberOfImages = document.querySelectorAll('.carouselImg img').length;
+let imageIndex = 1;
+let translateX = 0;
+
+carouselBtn.forEach(button => {
+  button.addEventListener('click', event => {
+    if (event.target.id === 'previousBtn') {
+      if (imageIndex !== 1) { // if not the first image
+        imageIndex--;
+        translateX += 300; // setting new translateX variable
+      }
+    } else {
+      if (imageIndex !== numberOfImages) { // if not the last image
+        imageIndex++;
+        translateX -= 300; // setting new translateX variable
+      }
+    }
+    carouselImg.style.transform = `translateX(${translateX}px)`; // updating the transform property of carouselImg container to the new current translateX variable from above
+  });
+});
